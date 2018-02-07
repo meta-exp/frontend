97a4ff83ea7fe096805e02a21fc0000a32840b6c
Freya Behrens <freya@fitt-behrens.de>
2018-02-07 17:23:01 +0100

    Refactor the Display

--- pika/src/Components/MetaPathComponents/MetaPathDisplay.js
+++ pika/src/Components/MetaPathComponents/MetaPathDisplay.js
@@ -1,67 +1,71 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import MetaPath from './MetaPath';
 import MetaPathID from './MetaPathID';
 import MetaPathRater from './MetaPathRater';
 
 class MetaPathDisplay extends Component {
 
-	getNextMetaPathBatch(){
-		this.getJsonFromBackend('next-meta-paths',this.addNewMetaPaths.bind(this));
-	}
+    /*
+        UI state handling
+    */
 
-	addNewMetaPaths(metapaths){
-		let oldMetaPaths = this.state.metapaths.slice();
-		oldMetaPaths = oldMetaPaths.concat(metapaths);
-		this.setState({metapaths: oldMetaPaths});
-	}
+    constructor(props) {
+        super();
+        this.state = {
+            metapaths: [],
+            ratedPaths: [],
+            nameIsSet: 0,
+            userName: "Davide",
+            similarityType: "Geolocation"
+        };
+    }
 
-	constructor(props){
-		super();
-		this.runningId = -1;
-		this.state = {
-			metapaths: [],
-			ratedPaths: [],
-			nameIsSet: 0,
-			userName: "Davide",
-			similarityType: "Geolocation"
-		};
-	}
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
 
-	handleRatingChange(event, id){
-		const metapaths = this.state.metapaths.slice();
-		let index = this.state.metapaths.findIndex(x => x.id === id);
-		metapaths[index].rating = event.target.value;
-		this.setState({metapaths: metapaths});
-	}
-
-	makeInteractiveRow(metaPath){
-		return (
-			<tr key={metaPath.id}>
-					<td><MetaPathID id={metaPath.id}/></td>
-					<td><MetaPath path={metaPath.path}/></td>
-					<td><MetaPathRater id={metaPath.id} defaultRating={metaPath.rating} rating={metaPath.rating} onChange={this.handleRatingChange.bind(this)}/></td>
-			</tr>
-		);
-	}
-
-	makeRatedRow(metaPath){
-		return (
-			<tr>
-					<td><MetaPathID id={metaPath.id}/></td>
-					<td>{metaPath.rating}</td>
-			</tr>
-		);
-	}
-
-    getJsonFromBackend(endpoint,callback) {
-        fetch('http://localhost:8000/'+endpoint, {
-            method: 'GET'
-        }).then((response) => response.json()).then(callback).catch((error) => {
-            console.error(error);
+        this.setState({
+            [name]: value
         });
     }
 
-		postToBackend(endpoint, data) {
+    handleRatingChange(event, id) {
+        const metapaths = this.state.metapaths.slice();
+        let index = this.state.metapaths.findIndex(x => x.id === id);
+        metapaths[index].rating = event.target.value;
+        this.setState({metapaths: metapaths});
+    }
+
+    /*
+        Backend Interaction
+    */
+
+    saveAllMetaPaths() {
+        alert("Not implemented yet.");
+    }
+
+    getNextMetaPathBatch() {
+        this.getJsonFromBackend('next-meta-paths', this.addNewMetaPathsToDisplay.bind(this));
+    }
+
+    addNewMetaPathsToDisplay(metapaths) {
+        let oldMetaPaths = this.state.metapaths.slice();
+        oldMetaPaths = oldMetaPaths.concat(metapaths);
+        this.setState({metapaths: oldMetaPaths});
+    }
+
+    getJsonFromBackend(endpoint, callback) {
+        fetch('http://localhost:8000/' + endpoint, {
+            method: 'GET'
+        }).then((response) => response.json()
+        ).then(callback).catch((error) => {
+            console.error(error);
+        })
+        ;
+    }
+
+    postJsonToBackend(endpoint, data) {
         fetch('http://localhost:8000/' + endpoint, {
             method: 'POST',
             headers: {
@@ -72,116 +76,145 @@ class MetaPathDisplay extends Component {
                 data
             }),
         }).then((response) => {
-					if(!(response.status === 200)){
-						alert('Could not send rating to server.');
-										}
-				}).catch((error) => {
-					console.error(error);
-				});
-	}
+            if (!(response.status === 200)
+            ) {
+                alert('Could not send data to server.');
+                console.log(data);
+            }
+        }).catch((error) => {
+            console.error(error);
+        })
+        ;
+    }
 
-	sendFeedback(){
-		let newRatedPaths = this.state.metapaths.map(path => ({id: path.id, rating: path.rating}));
-		let ratedPaths = this.state.ratedPaths.slice();
-		ratedPaths = ratedPaths.concat(newRatedPaths);
-		this.setState({ratedPaths: ratedPaths, metapaths: []});
-		this.getNextMetaPathBatch();
-		this.postToBackend('rate-meta-paths', newRatedPaths);
-		}
+    nextRatingIteration() {
+        let newRatedPaths = this.state.metapaths.map(path => ({id: path.id, rating: path.rating}));
+        let ratedPaths = this.state.ratedPaths.slice();
+        ratedPaths = ratedPaths.concat(newRatedPaths);
+        this.setState({ratedPaths: ratedPaths, metapaths: []});
+        this.getNextMetaPathBatch();
+        this.postJsonToBackend('rate-meta-paths', newRatedPaths);
+    }
 
-	savePaths(){
-		alert("Not implemented yet.");
-	}
 
-	submitNaming(){
-		this.setState({
-      nameIsSet: 1
-    });
+    submitNaming() {
+        this.postJsonToBackend('submit-name', {purpose: this.state.submitNaming, name: this.state.userName});
+        this.setState({
+            nameIsSet: 1
+        });
+    }
 
-	}
+    /*
+        Methods for rendering the html
+    */
 
-	handleInputChange(event){
-		const target = event.target;
-		const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+    render() {
+        if (this.state.nameIsSet === 0) {
+            return this.renderNaming();
+        } else {
+            return this.renderWeighting();
+        }
 
-		this.setState({
-      [name]: value
-    });
-	}
+    }
 
-	renderWeighting(){
-		let tableRows = this.state.metapaths.map((path, index) => this.makeInteractiveRow(path, index));
-		let ratedPaths = this.state.ratedPaths.map(path => this.makeRatedRow(path));
+    renderWeighting() {
+        let tableRows = this.state.metapaths.map((path, index) => this.renderMetaPathRatingRow(path, index));
+        let ratedPaths = this.state.ratedPaths.map(path => this.renderRatedMetaPathRow(path));
 
-		return (
-			<div>
-			<div>
-				<h4> Prurpose: </h4> {this.state.similarityType} <br />
-				<h4> by: </h4> {this.state.userName}
-			</div>
-			<h3 align='center' className="font-weight-bold">Found Meta Paths</h3>
-			<table align="center">
-				<thead>
-				<tr>
-					<td>ID</td>
-					<td>Path</td>
-					<td>Rating</td>
-					</tr>
-				</thead>
-				<tbody>
-				{tableRows}
-				<tr>
-						<td colSpan="3">
-							<div className="row">
-								<button className="btn btn-primary mx-auto" id="show-more-meta-paths-btn" onClick={()=>this.sendFeedback()}>
-									<span>Confirm Current Rating & Get Next -> </span>
-								</button>
-								<button className="btn btn-primary mx-auto" id="show-more-meta-paths-btn" onClick={this.savePaths.bind(this)}>
-								<span>Save Rating</span>
-								</button>
-							</div>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<h3 align='center' className="font-weight-bold">Rated Meta Paths</h3>
-			<table align="center">
-			<thead>
-			<tr>
-				<td>ID</td>
-				<td>Rating</td>
-				</tr>
-			</thead>
-			<tbody>
-			{ratedPaths}
-			</tbody>
-		</table>
-			</div>
-		);
-	}
+        return (
+            <div>
+                <div>
+                    <h4> Purpose: </h4> {this.state.similarityType} <br/>
+                    <h4> Created by: </h4> {this.state.userName}
+                </div>
+                <h3 align='center' className="font-weight-bold"> Found Meta Paths </h3>
+                <table align="center">
+                    <thead>
+                    <tr>
+                        <td> ID</td>
+                        <td> Path</td>
+                        <td> Rating</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {tableRows}
+                    <tr>
+                        <td colSpan="3">
+                            <div className="row">
+                                <button className="btn btn-primary mx-auto"
+                                        id="show-more-meta-paths-btn"
+                                        onClick={this.nextRatingIteration.bind(this)}>
+                                    <span> Confirm Current Rating & Get Next </span>
+                                </button>
+                                <button className="btn btn-primary mx-auto"
+                                        id="show-more-meta-paths-btn"
+                                        onClick={this.saveAllMetaPaths.bind(this)}>
+                                    <span> Save Rating </span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <h3 align='center'
+                    className="font-weight-bold"> Rated Meta Paths </h3>
+                <table align="center">
+                    <thead>
+                    <tr>
+                        <td> ID</td>
+                        <td> Rating</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {ratedPaths}
+                    </tbody>
+                </table>
+            </div>
+        )
+            ;
+    }
 
-	renderNaming(){
-		return (<div>
-			<label htmlFor="uname">Your Name: </label>
-			<input type="text" id="uname" name="userName" value={this.state.userName} onChange={this.handleInputChange.bind(this)}/>
-			<br />
-			<label htmlFor="uname">Describe the type of similarity: </label>
-			<input type="text" id="simtype" name="similarityType" value={this.state.similarityType} onChange={this.handleInputChange.bind(this)}/>
-			<div>
-    			<button onClick={this.submitNaming.bind(this)}>Submit</button>
-  		</div>
-		</div>);
-	}
+    renderNaming() {
+        return (<div>
+            <label htmlFor="uname"> Your Name: </label>
+            <input type="text"
+                   id="uname"
+                   name="userName"
+                   value={this.state.userName}
+                   onChange={this.handleInputChange.bind(this)}/>
+            <br/>
+            <label htmlFor="uname"> Describe the type of similarity: </label>
+            <input type="text"
+                   id="simtype"
+                   name="similarityType"
+                   value={this.state.similarityType}
+                   onChange={this.handleInputChange.bind(this)}/>
+            <div>
+                <button onClick={this.submitNaming.bind(this)}>Submit</button>
+            </div>
+        </div>);
+    }
 
-	render() {
-		if(this.state.nameIsSet === 0){
-			return this.renderNaming();
-		} else {
-			return this.renderWeighting();
-		}
 
-	}
+    renderMetaPathRatingRow(metaPath) {
+        return (
+            <tr key={metaPath.id}>
+                <td><MetaPathID id={metaPath.id}/></td>
+                <td><MetaPath path={metaPath.path}/></td>
+                <td>< MetaPathRater id={metaPath.id} defaultRating={metaPath.rating} rating={metaPath.rating}
+                                    onChange={this.handleRatingChange.bind(this)}/></td>
+            </tr>
+        );
+    }
+
+    renderRatedMetaPathRow(metaPath) {
+        return (
+            <tr>
+                <td>< MetaPathID id={metaPath.id}/></td>
+                <td> {metaPath.rating}</td>
+            </tr>
+        );
+    }
 
 }
 
