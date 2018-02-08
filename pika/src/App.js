c2fda32f3bedb6a7b37d6c69ecef7962cc8fd86e
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-02-08 11:22:47 +0100

    divide components into subfolders

--- pika/src/App.js
+++ pika/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import Nav from './Components/Nav';
-import Footer from './Components/Footer';
+import Nav from './Components/NavComponents/Nav';
+import Footer from './Components/FooterComponents/Footer';
 
 import Setup from './Components/SetupComponents/Setup';
 import Config from './Components/Config';
