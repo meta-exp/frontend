c4506e87b33b534ea977bfc9ac5c9a6ff3d8cbe4
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-04-03 17:11:53 +0200

    Make UI runnable again

--- pika/src/containers/AppContainer.js
+++ pika/src/containers/AppContainer.js
@@ -2,30 +2,22 @@ import AppView from '../views/AppView';
 import {Container} from 'flux/utils';
 import AccountStore from '../data/AccountStore';
 import AccountActions from '../data/AccountActions';
-import MetaPathRateBatchStore from '../data/MetaPathRateBatchStore';
-import MetaPathRateBatchActions from '../data/MetaPathRateBatchActions';
 
 
 function getStores(){
   return  [
-    AccountStore,
-    MetaPathRateBatchStore
+    AccountStore
   ];
 }
 
 function getState(){
   return {
     account: AccountStore.getState(),
-    metaPathRateBatch: MetaPathRateBatchStore.getState(),
-
     loadDatasets: AccountActions.loadDatasets,
-
     onUpdateUsername: AccountActions.updateUsername,
     onLogin: AccountActions.login,
     onLogout: AccountActions.logout,
-    onDatasetSelection: AccountActions.selectDataset,
-    onBatchSave: MetaPathRateBatchActions.batchSave,
-    onNextBatch: MetaPathRateBatchActions.getNext
+    onDatasetSelection: AccountActions.selectDataset
   };
 }
 
