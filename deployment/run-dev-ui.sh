fff32261fd116ab387e1395da84a3311fd67d8e4
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-04-09 14:54:58 +0200

    Fix API Host in development and production

--- deployment/run-dev-ui.sh
+++ deployment/run-dev-ui.sh
@@ -2,9 +2,9 @@
 docker stop ui-dev-container
 docker rm ui-dev-container
 docker run --name ui-dev-container \
-           --publish=${2:-3000}:3000 \
+           --publish=${1:-3000}:3000 \
            -d \
-           -e REACT_APP_API_HOST=$1 \
+           -e REACT_APP_API_HOST=$REACT_APP_API_HOST \
            dev-ui \
            npm start
            
